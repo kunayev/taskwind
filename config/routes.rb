139da82bdc106f55afa93c662b58b7866de443f9
Prelang Builder <builder@prelang.com>
2014-03-16 01:52:25 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Taskwind::Application.routes.draw do
+  get "landings/index"
   resources :projects
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
